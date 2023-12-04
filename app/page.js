8dfa5a0777c83680d94a1a85339d1016a8194fd8
pradeepgr744 <dseeeap2020@gmail.com>
2023-12-04 14:06:50 +0530

    update

--- app/page.js
+++ app/page.js
@@ -44,42 +44,42 @@ const page = () => {
                 className='noselect blue'
                 onClick={() => dis('7')}
               >7</button>
-              <button type="button" onClick={() => dis('8')} className='noselect blue'
+              <button type="button" onClick={() => dis('8')} className='noselect red'
               >8</button>
-              <button type="button" onClick={() => dis('9')} className='noselect blue'
+              <button type="button" onClick={() => dis('9')} className='noselect green'
               >9</button>
-              <button type="button" onClick={() => dis('/')} className='noselect blue'
+              <button type="button" onClick={() => dis('/')} className='noselect purple'
               >/</button>
             </div>
             <div>
-              <button type="button" onClick={() => dis('4')} className='noselect blue'
+              <button type="button" onClick={() => dis('4')} className='noselect red'
               >6</button>
               <button type="button" onClick={() => dis('5')} className='noselect blue'
               >5</button>
-              <button type="button" onClick={() => dis('6')} className='noselect blue'
+              <button type="button" onClick={() => dis('6')} className='noselect red'
               >4</button>
-              <button type="button" onClick={() => dis('*')} className='noselect blue'
+              <button type="button" onClick={() => dis('*')} className='noselect green'
               >x</button>
             </div>
             <div>
               <button type="button" onClick={() => dis('1')} className='noselect blue'
               >1</button>
-              <button type="button" onClick={() => dis('2')} className='noselect blue'
+              <button type="button" onClick={() => dis('2')} className='noselect red'
               >2</button>
               <button type="button" onClick={() => dis('3')} className='noselect blue'
               >3</button>
-              <button type="button" onClick={() => dis('-')} className='noselect blue'
+              <button type="button" onClick={() => dis('-')} className='noselect red'
               >-</button>
             </div>
             <div>
               <button type="button" onClick={() =>
-                setNumber("")} className='noselect blue'
+                setNumber("")} className='noselect purple'
               >C</button>
-              <button type="button" onClick={() => dis('0')} className='noselect blue'
+              <button type="button" onClick={() => dis('0')} className='noselect green'
               >0</button>
               <button type="button" onClick={() =>
                 setNumber(eval(Number))
-              } className='noselect blue'
+              } className='noselect red'
               >=</button>
               <button type="button" onClick={() => dis('+')} className='noselect blue'
               >+</button>
